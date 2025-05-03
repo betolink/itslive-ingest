cc74cdcdc11d4fba7a010979521d89ae488d6eae
betolink <betolin@gmail.com>
2025-05-02 20:57:57 -0500

    fix image

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Activate the conda environment
+set -e
 
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --log-level info
+exec micromamba run -p /opt/conda uvicorn main:app --host 0.0.0.0 --port 8000 --log-level info
+
 
